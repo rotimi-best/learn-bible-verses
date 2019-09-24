ffa063bb6a0988ddd0b614a9631b8e11b62268e3
Best <irb.ossystem@gmail.com>
2019-09-24 20:42:31 +0300

    feat: set up db config + dotenv example

---
+++ helpers/dbConfig.js
@@ -0,0 +1,9 @@
+require('dotenv').config()
+
+const ENV = process.env
+
+module.exports = {
+    MONGO_URI: `${ENV.DB_PROVIDER}://${ENV.DB_USER}:${encodeURIComponent(
+        ENV.DB_PWORD
+    )}@${ENV.DB_HOST}/${ENV.DB_NAME}`,
+}
