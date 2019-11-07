052fa64b869f8e46d1d455ee13e11527c5eb833a
Best <irb.ossystem@gmail.com>
2019-11-08 01:08:46 +0200

    fix: make logger short

--- db/connect.js
+++ db/connect.js
@@ -1,28 +1,33 @@
 const mongoose = require('mongoose')
 const { MONGO_URI } = require('../helpers/dbConfig')
-const { log } = require('../services/loggerService')
+const {
+    info: infoLogger,
+    error: errorLogger,
+} = require('../services/loggerService')
 
 module.exports = async () => {
     try {
-        await mongoose.connect(MONGO_URI, {
+        mongoose.connect(MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         })
     } catch (error) {
-        log('error', error.stack.toString())
-        log('error', `${__filename} ${__line} DATABASE CONNECTION ERROR`)
+        errorLogger(
+            `${__filename}\n${__line}\nDATABASE CONNECTION ERROR\n${error.stack.toString()}`
+        )
     }
 
     mongoose.Promise = global.Promise
     const db = mongoose.connection
 
     db.on('error', error => {
-        log('error', error.stack.toString())
-        log('error', `${__filename} ${__line} DATABASE CONNECTION ERROR`)
+        errorLogger(
+            `${__filename}\n${__line}\nDATABASE CONNECTION ERROR\n${error.stack.toString()}`
+        )
     })
 
     db.once('open', () => {
-        log('info', 'DATABASE HAS SUCCESSFULLY BEING OPENED')
+        infoLogger('DATABASE HAS SUCCESSFULLY BEING OPENED')
     })
 
     return db
